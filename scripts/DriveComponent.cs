891ceff375842bdfbf73d6982b0b2b497459f5f1
silktail <silktail@silktop.dev>
2025-02-07 02:44:21 -0500

    3 days

--- scripts/DriveComponent.cs
+++ scripts/DriveComponent.cs
@@ -1,62 +1,88 @@
 using Godot;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 /// <summary>
 /// drive docs:
 /// expl: should be able to cache wheel positions and keep them upon loading. car will always attempt to push wheels to og position. works by raycasting down from above og wheel pos looking only for the ground, when it finds it it will use the wheels orientation+the last ray used+the closest ray to true down, this gets details on grip/height/suspension/and current speed. each wheel will have an output force on the car, originating from a suspension connection point specified in the wheel
-/// TODO: implement rigidbody, spinning wheels, moving, suspension
+/// TODO: implement rigidbody, spinning wheels, moving, suspension, rotational inertia
 /// only ONE RIGIDBODY
 /// </summary>
 public partial class DriveComponent : Node
 {	
-	public float mass {get; set;}
-	public float brake {get; set;}
-	public float gas {get; set;}
+	public Vector3 lastGroundNormal;
+	public bool OnGround{get{
+		var groundQuery = PhysicsRayQueryParameters3D.Create(car.GlobalPosition,car.GlobalPosition-car.GlobalBasis.Y.Normalized()*10f);
+    	var groundResult = DriveComponent.globalState.IntersectRay(groundQuery);
+		return groundResult.Count>0;
+	}}
+	public float engine_ratio;
+	public float slowdown_factor;
+	public float acc;
+	public float mass;
+	public float brake;
+	public float gas;
 	public RigidBody3D car;
 	public static PhysicsDirectSpaceState3D globalState;
 	// Called when the node enters the scene tree for the first time.
-	public Dictionary<Node,Vector3> IntendedWheelPositions;
-	public override void _EnterTree()
+	public Dictionary<Wheel,Vector3> IntendedWheelPositions;
+	public override void _Ready()
 	{
 		car = (RigidBody3D)GetParent();
 		IntendedWheelPositions = new();
-		var children = GetChildren();
-		foreach(Node3D wheel in GetChildren().Where(obj => obj.Name.ToString().Contains("Wheel")))
+		var children = car.GetChildren();
+		foreach(Wheel wheel in children.Where(obj => obj is Wheel))
 		{
-			IntendedWheelPositions.Add(wheel,wheel.GlobalPosition);
+			IntendedWheelPositions.Add(wheel,wheel.Position);
 		}
+		car.AngularDamp = .5f;
 	}
-	public void ApplySuspensionForce(Wheel w){
-		var force = w.SuspensionForce(IntendedWheelPositions[w],this);
-		var wheelLen = (((Node3D)w.GetChild(0)).GlobalPosition-w.GlobalPosition).Length();
-		var offset = wheelLen*Vector3.Down*car.Basis;
-		var RayOffset = offset+IntendedWheelPositions[w]-w.GlobalPosition;
-		var groundQuery = PhysicsRayQueryParameters3D.Create(w.GlobalPosition,w.GlobalPosition+RayOffset);
-    	var groundResult = DriveComponent.globalState.IntersectRay(groundQuery);
-		if(groundResult.Count>0)
-		{
-			w.GlobalPosition=(Vector3)groundResult["position"]-offset;
-		}
-		else
-		{
-			w.GlobalPosition=IntendedWheelPositions[w];
-		}
-		car.ApplyForce(force,w.GlobalPosition);
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+		_Ready();
+    }
+    public void ApplySuspensionForce(Wheel wheel){
+		var f = wheel.SuspensionForce(IntendedWheelPositions[wheel],this);
+		car.ApplyForce(f,wheel.Position);
 	}
-	public void ApplyForwardForce(Wheel w){
-		
-		car.ApplyForce(w.ForwardForce(this),w.LastGripPoint);
+	public void ApplyForwardForce(Wheel w,double delta){
+		//rotation speed is a compound of old rotation speed and gas capped by engine ratio @ 9000k each engine will have a different gas injection pattern(not as realistic as it could be kill me)
+		w.rotationSpeed = Mathf.Clamp((gas*2*Mathf.Pi*acc+w.rotationSpeed)*slowdown_factor,-9000/engine_ratio,9000/engine_ratio);
+		//moving the wheel is the first nessicary step to get the speed to apply, missing framerate should theoretically be accounted for since apply force uses delta t and so do all needed functions(looking @ rotating wheels!!!)
+		w.Rotate(w.GlobalBasis.Column2.Normalized(),(float)(w.rotationSpeed*delta));
+		var force = w.ForwardForce(this);
+		//car.ApplyImpulse(force,w.LastGripPoint);
+		//change rotation speed based on how car body responds to forces
+		w.rotationSpeed+=(car.LinearVelocity-force).Dot(car.GlobalBasis.Column2);
 	}
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
-		bool setspace = false;
+		car.ConstantForce = Vector3.Zero;
 		foreach(Wheel wheel in IntendedWheelPositions.Keys)
 		{
-			if(!setspace)
-			globalState = PhysicsServer3D.SpaceGetDirectState(wheel.GetWorld3D().Space);
-			setspace = true;
 			
+			globalState = PhysicsServer3D.SpaceGetDirectState(wheel.GetWorld3D().Space);
+			ApplySuspensionForce(wheel);
+			//ApplyForwardForce(wheel,delta);
+			//car.GlobalPosition = Vector3.Up*30;
+			//car.Rotate(Vector3.Up,.02f); 
+			
+			if(gas==0&&OnGround)
+			{
+				var intended = car.LinearVelocity.Dot(car.GlobalBasis.Y)*car.GlobalBasis.Y;
+				var offset = intended-car.LinearVelocity;
+				if(offset.Length()<.03){
+					car.LinearVelocity=intended;
+				}
+				else{
+					offset = offset.Normalized()*.03f;
+					car.LinearVelocity+=offset;
+				}
+			}
 		}
+		
     }
 }
--- scripts/Wheel.cs
+++ scripts/Wheel.cs
@@ -1,17 +1,31 @@
 using Godot;
+using Godot.Collections;
 using System.Linq;
-using System;
-using System.Net.WebSockets;
 //TODO implement typed GetChildren!!!!!!!!!
-public partial class Wheel : Node3D
+public partial class Wheel : MeshInstance3D
 {
+	public MeshInstance3D f;
+	public MeshInstance3D t;
+	public PhysicsDirectSpaceState3D space;
+	//curremnt rotation speed
+	
+	public float floatiness=.4f;
 	public float rotationSpeed;
-	[Export]
-	public float suspension_coefficient;
+	public float fall_coefficient=2f;
+	public float suspension_coefficient=1f;
 	[Export]
 	public float mass;
+	public Vector3 LastDownPoint{ get; set;}
 	public Vector3 LastGripPoint{ get; set;}
 	public Node3D LastUsed{ get; set;}
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+    }
     public Node3D MostDown(DriveComponent driveComponent)
 	{
 		var down = Vector3.Down*driveComponent.car.GlobalBasis;
@@ -19,21 +33,52 @@ public partial class Wheel : Node3D
             (-down).Dot(GlobalPosition-((Node3D)e).GlobalPosition)
 			).First();
 	}
-	public Vector3 SuspensionForce(Vector3 intended,DriveComponent driveComponent){
-		var down = Vector3.Down*driveComponent.car.Basis;
-		down*=(((Node3D)GetChildren().First()).GlobalPosition-GlobalPosition).Length();
-		down+=intended;
-		var groundQuery = PhysicsRayQueryParameters3D.Create(GlobalPosition,down);
+	public Vector3 SuspensionForce(Vector3 intend,DriveComponent driveComponent){
+		var parent = driveComponent.car;
+		Vector3 intended = parent.GlobalBasis.Z * intend.Z + parent.GlobalBasis.X * intend.X + parent.GlobalBasis.Y * intend.Y + parent.GlobalPosition;
+		var wtc = driveComponent.car.GlobalPosition-intended;
+
+		var up = parent.GlobalBasis.Y.Normalized();
+		var force = Vector3.Zero;
+		var wheelLen = (((Node3D)this.GetChild(0)).GlobalPosition-this.GlobalPosition).Length();
+		var offset = wheelLen*-up;
+
+        var groundQuery = PhysicsRayQueryParameters3D.Create(intended + up*wheelLen, intended+offset,exclude: new Array<Rid>(new Rid[1] {driveComponent.car.GetRid()}) );
     	var groundResult = DriveComponent.globalState.IntersectRay(groundQuery);
+		if(f == null)
+		 f = Draw3D.Point(intended+up*20,1,Colors.Red);
+		else
+		 f.GlobalPosition = intended+up*20;
+		if(t == null)
+		 t = Draw3D.Point(intended + offset,1,Colors.Beige);
+		else
+		 t.GlobalPosition = intended + offset;
+		if(groundResult.Count>0){
+			force = suspension_coefficient*((((Vector3)groundResult["position"]-(intended+offset)).Length()*wtc)*floatiness+(1-floatiness)*((Vector3)groundResult["position"]-(intended+offset)));
+		}
+		else{
+			driveComponent.car.AngularVelocity = Vector3.Zero;
+		}
+			//force = ((Vector3)groundResult["position"]-(intended+offset)).Length()*suspension_coefficient*((((Vector3)groundResult["position"]-(intended+offset)).Length()*wtc)*floatiness+(1-floatiness)*((Vector3)groundResult["position"]-(intended+offset)));
+		
 		if(groundResult.Count>0)
-		return ((intended-GlobalPosition).Normalized()*suspension_coefficient)/(intended-GlobalPosition).Length();
-		return Vector3.Zero;
+		{
+			GlobalPosition=(Vector3)groundResult["position"]-offset;
+		}
+		else
+		{
+			GlobalPosition=driveComponent.IntendedWheelPositions[this];
+		}
+		//apply the forces to the car
+		GD.Print(Name,force);
+		return force;
 	}
+
 	public Vector3 ForwardForce(DriveComponent driveComponent){
 		var rotationalQuery = PhysicsRayQueryParameters3D.Create(GlobalPosition,GlobalPosition+2*(LastUsed.GlobalPosition-GlobalPosition));
     	var rotationalResult = DriveComponent.globalState.IntersectRay(rotationalQuery);
 		var down = Vector3.Down*driveComponent.car.Basis;
-		down*=(((Node3D)GetChildren().First()).GlobalPosition-GlobalPosition).Length();
+		down*=4.4f;
 		var groundQuery = PhysicsRayQueryParameters3D.Create(GlobalPosition,GlobalPosition+down);
     	var groundResult = DriveComponent.globalState.IntersectRay(groundQuery);
 		if(rotationalResult.Count>0)
@@ -46,7 +91,7 @@ public partial class Wheel : Node3D
     			rotationalResult = DriveComponent.globalState.IntersectRay(rotationalQuery);
 				LastGripPoint = (Vector3)rotationalResult["position"];
 				
-				return (LastGripPoint-position);
+				return (LastGripPoint-position)-driveComponent.car.LinearVelocity;
 			}
 		}
 		LastUsed = MostDown(driveComponent);
